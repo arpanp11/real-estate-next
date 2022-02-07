9751e42e78b3f9e8ef4162b4759d5b7d3c6b616c
arpanp11 <arpanpatel2810@gmail.com>
2022-02-07 13:56:14 -0500

    added search location function

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -14,10 +14,15 @@ import Image from 'next/image';
 import { MdCancel } from 'react-icons/md';
 
 import { filterData, getFilterValues } from '../utils/filterData';
+import { baseUrl, fetchApi } from '../utils/fetchApi';
+import noresult from '../assets/images/noresult.svg';
 
 const SearchFilters = () => {
   const [filters, setFilters] = useState(filterData);
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchText, setSearchText] = useState('');
+  const [location, setLocation] = useState();
+  const [showLocations, setShowLocations] = useState(false);
+  const [loading, setLoading] = useState(false);
   const router = useRouter();
 
   //   search property handler
@@ -36,6 +41,23 @@ const SearchFilters = () => {
 
     router.push({ pathname: path, query });
   };
+
+  useEffect(() => {
+    if (searchText !== '') {
+      const fetchLocation = async () => {
+        setLoading(true);
+
+        const data = await fetchApi(
+          `${baseUrl}/auto-complete?query=${searchText}`
+        );
+        // console.log(data);
+        setLoading(false);
+        setLocation(data?.hits);
+      };
+
+      fetchLocation();
+    }
+  }, [searchText]);
 
   return (
     <Flex bg='gray.100' p='4' justifyContent='center' flexWrap='wrap'>
@@ -57,6 +79,87 @@ const SearchFilters = () => {
           </Select>
         </Box>
       ))}
+
+      <Flex flexDir='column'>
+        <Button
+          border='1px'
+          borderColor='gray.200'
+          marginTop='2'
+          bg='blue.400'
+          onClick={() => setShowLocations(!showLocations)}
+        >
+          Search Location
+        </Button>
+
+        {/* spinner */}
+        {loading && <Spinner margin='auto' marginTop='3' />}
+
+        {showLocations && (
+          <Flex flexDir='column' pos='relative' paddingTop='2'>
+            {/* input  */}
+            <Input
+              placeholder='Type Location Here'
+              value={searchText}
+              w='300px'
+              focusBorderColor='gray.300'
+              onChange={(e) => setSearchText(e.target.value)}
+            />
+            {searchText !== '' && (
+              <Icon
+                as={MdCancel}
+                pos='absolute'
+                cursor='pointer'
+                right='5'
+                top='5'
+                zIndex='100'
+                onClick={() => setSearchText('')}
+              />
+            )}
+
+            {showLocations && (
+              <Box height='300px' overflow='auto'>
+                {location?.map((l) => (
+                  <Box
+                    key={l.id}
+                    onClick={() => {
+                      searchProperties({
+                        locationExternalIDs: l.externalID,
+                      });
+                      setShowLocations(false);
+                      setSearchText(l.name);
+                    }}
+                  >
+                    <Text
+                      cursor='pointer'
+                      bg='gray.200'
+                      p='2'
+                      borderBottom='1px'
+                      borderColor='gray.100'
+                    >
+                      {l.name}
+                    </Text>
+                  </Box>
+                ))}
+                {/* no location found, load noresult image here */}
+                {!loading && !location?.length && (
+                  <Flex
+                    justifyContent='center'
+                    alignItems='center'
+                    flexDir='column'
+                    marginTop='5'
+                    marginBottom='5'
+                  >
+                    <Image src={noresult} alt='no result' />
+                    <Text fontSize='xl' marginTop='3'>
+                      Waiting to search!
+                    </Text>
+                  </Flex>
+                )}
+              </Box>
+            )}
+          </Flex>
+        )}
+      </Flex>
     </Flex>
   );
 };
